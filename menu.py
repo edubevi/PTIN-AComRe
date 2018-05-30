7ed59430a9bab26bd6ba54705c9d5a24b2ea6c43
pol <stopenheaven@gmail.com>
2018-05-30 21:02:17 +0200

    stop containers READY

--- menu.py
+++ menu.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+
 from tkinter import *
 import time, docker, subprocess, sys, json
 
-devices = {"doctor": 1, "patient": 2, "ambulance": 3, "smoke": 4, "weather": 5}
-num_devices = {"doctor": 0, "patient": 0, "ambulance": 0, "smoke": 0, "weather": 0}
+# List of allowed devices with their type id.
+devices = {"doctor":1, "patient":2, "ambulance":3,"smoke":4,"weather":5,"air":6, "nurse":7, "stretcher":8}
+# stores the number of devices by type.
+num_devices = {"doctor":0, "patient":0, "ambulance":0,"smoke":0,"weather":0,"air":0, "nurse":0, "stretcher":0}
 
 
 def runcontainers():
@@ -31,7 +35,7 @@ def runcontainers():
             c_type = "-t " + str(devices[type])
             container = client.containers.run("peremontpeo/virtualdevices:latest", c_type, detach=True, name=c_name,
                                               auto_remove=True)
-            text_ = "+ Container with short_id=" + container.short_id + " has been created.\n"
+            text_ = " + Container with short_id=" + container.short_id + " has been created.\n"
 
             t.insert(INSERT, text_)
             t.pack()
@@ -44,7 +48,7 @@ def runcontainers():
     # Create window menu create containers
     dialeg1 = Toplevel(window)
     dialeg1.title("Run Container/s")
-    dialeg1.geometry("270x165+550+300")
+    dialeg1.geometry("270x180+530+300")
 
     # Add label
     runcont = StringVar()
@@ -58,20 +62,23 @@ def runcontainers():
     Radiobutton(dialeg1, text="Doctor", variable=text_info2, value='doctor').place(x=10, y=50)
     Radiobutton(dialeg1, text="Ambulance", variable=text_info2, value='ambulance').place(x=90, y=30)
     Radiobutton(dialeg1, text="Smoke", variable=text_info2, value='smoke').place(x=90, y=50)
-    Radiobutton(dialeg1, text="Weather", variable=text_info2, value='weather').place(x=180, y=50)
+    Radiobutton(dialeg1, text="Weather", variable=text_info2, value='weather').place(x=190, y=30)
+    Radiobutton(dialeg1, text="Air", variable=text_info2, value='air').place(x=190, y=50)
+    Radiobutton(dialeg1, text="Nurse", variable=text_info2, value='nurse').place(x=50, y=70)
+    Radiobutton(dialeg1, text="Stretcher", variable=text_info2, value='stretcher').place(x=140, y=70)
 
     # Add label
     runcont2 = StringVar()
     runcont2.set("Number of replicas:")
-    Label(dialeg1, textvariable=runcont2).place(x=10, y=80)
+    Label(dialeg1, textvariable=runcont2).place(x=10, y=100)
 
     # Add text for introduce numbers
     text2 = StringVar()
-    Entry(dialeg1, textvariable=text2, width=30).place(x=10, y=100)
+    Entry(dialeg1, textvariable=text2, width=32).place(x=10, y=120)
 
     # Add buttoms for create this containers or close window
-    Button(dialeg1, text='Create', command=boton_ok).place(x=60, y=130)
-    Button(dialeg1, text='Close', command=dialeg1.destroy).place(x=150, y=130)
+    Button(dialeg1, text='Create', command=boton_ok).place(x=65, y=150)
+    Button(dialeg1, text='Close', command=dialeg1.destroy).place(x=155, y=150)
 
 
 def stop_all():
@@ -97,38 +104,100 @@ def stop_all():
         print("+ Stopping container/s....")
         for container in container_list:
             container.stop()
-            text_ = "+ Container with short_id=" + container.short_id + " has been stopped.\n"
+            text_ = " + Container with short_id=" + container.short_id + " has been stopped.\n"
             t.insert(INSERT, text_)
             t.pack()
             print(text_)
         for key in num_devices.keys(): num_devices[key] = 0  # updates the number of all devices to 0
 
+def stopped_containers():
 
-# def stop_containers(client, num, type):
-#     # container_list = client.containers.list(filters={'name':'doctor_*'})
-#     # llista amb el short_id dels contenidors que executa el host.
-#     container_list = [container.short_id for container in client.containers.list(filters={'name': type + "*"})]
-#     if len(container_list) == 0 or num == 0:
-#         print("There are no containers to stop.")
-#     elif len(container_list) <= num:
-#         # stop all running containers
-#         print("+ Stopping container/s....")
-#         for item in container_list:
-#             container = client.containers.get(item)
-#             container.stop()
-#             print("+ Container with short_id=" + item + " has been stopped.")
-#         num_devices[type] = 0  # updates the number of devices
-#     elif len(container_list) > num:
-#         # stop the latest num of containers.
-#         print("+ Stopping container/s....")
-#         stopped_container = 0
-#         while stopped_container != num:
-#             container = client.containers.get(container_list[0])
-#             container.stop()
-#             print("+ Container with short_id=" + container_list[0] + " has been stopped.")
-#             container_list.pop(0)
-#             num_devices[type] -= 1
-#             stopped_container += 1
+    def boton_stop():
+        dada1 = text_info3.get()
+        num = int(text2.get())
+        if dada1 in devices:
+            stop_containers(client, dada1, num)
+
+    def stop_containers(client, type, num):
+
+        # Create window for print containers creates
+        finestra = Toplevel()
+        finestra.title('Stopping container/s')
+        finestra.geometry("450x205+440+300")
+        # Add buttom close window
+        Button(finestra, text='Close', command=finestra.destroy).place(x=200, y=165)
+
+        # Add text for print
+        t = Text(finestra, height=10)
+
+        # container_list = client.containers.list(filters={'name':'doctor_*'})
+        # llista amb el short_id dels contenidors que executa el host.
+        container_list = [container.short_id for container in client.containers.list(filters={'name': type + "*"})]
+        if len(container_list) == 0 or num == 0:
+            text_ = "There are no containers to stop.\n"
+            t.insert(INSERT, text_)
+            t.pack()
+            print(text_)
+        elif len(container_list) <= num:
+            # stop all running containers
+            print("+ Stopping container/s....")
+            for item in container_list:
+                container = client.containers.get(item)
+                container.stop()
+                text_ = " + Container with short_id=" + item + " has been stopped.\n"
+                t.insert(INSERT, text_)
+                t.pack()
+                print(text_)
+            num_devices[type] = 0  # updates the number of devices
+        elif len(container_list) > num:
+            # stop the latest num of containers.
+            print("+ Stopping container/s....")
+            stopped_container = 0
+            while stopped_container != num:
+                container = client.containers.get(container_list[0])
+                container.stop()
+                text_ = " + Container with short_id=" + container_list[0] + " has been stopped.\n"
+                t.insert(INSERT, text_)
+                t.pack()
+                print(text_)
+                container_list.pop(0)
+                num_devices[type] -= 1
+                stopped_container += 1
+
+    # Create window menu create containers
+    dialeg2 = Toplevel(window)
+    dialeg2.title("Stop container/s")
+    dialeg2.geometry("280x180+530+300")
+
+    # Add label
+    runcont = StringVar()
+    runcont.set("Allowed device type:")
+    Label(dialeg2, textvariable=runcont).place(x=10, y=10)
+
+    text_info3 = StringVar()
+
+    # Introduce radiobuttoms for select type devices
+    Radiobutton(dialeg2, text="Patient", variable=text_info3, value='patient').place(x=10, y=30)
+    Radiobutton(dialeg2, text="Doctor", variable=text_info3, value='doctor').place(x=10, y=50)
+    Radiobutton(dialeg2, text="Ambulance", variable=text_info3, value='ambulance').place(x=90, y=30)
+    Radiobutton(dialeg2, text="Smoke", variable=text_info3, value='smoke').place(x=90, y=50)
+    Radiobutton(dialeg2, text="Weather", variable=text_info3, value='weather').place(x=190, y=30)
+    Radiobutton(dialeg2, text="Air", variable=text_info3, value='air').place(x=190, y=50)
+    Radiobutton(dialeg2, text="Nurse", variable=text_info3, value='nurse').place(x=50, y=70)
+    Radiobutton(dialeg2, text="Stretcher", variable=text_info3, value='stretcher').place(x=140, y=70)
+
+    # Add label
+    runcont2 = StringVar()
+    runcont2.set("Number of containers to stop:")
+    Label(dialeg2, textvariable=runcont2).place(x=10, y=100)
+
+    # Add text for introduce numbers
+    text2 = StringVar()
+    Entry(dialeg2, textvariable=text2, width=32).place(x=10, y=120)
+
+    # Add buttoms for create this containers or close window
+    Button(dialeg2, text='Stop', command=boton_stop).place(x=65, y=150)
+    Button(dialeg2, text='Close', command=dialeg2.destroy).place(x=155, y=150)
 
 
 def list_running_containers():
@@ -189,6 +258,21 @@ def list_running_containers():
                 t.insert(INSERT, text_)
                 t.pack()
                 print(text_,end='\t')
+            elif "air" in container.name:
+                text_ = "\tair" + '\t\t'
+                t.insert(INSERT, text_)
+                t.pack()
+                print(text_,end='\t')
+            elif "nurse" in container.name:
+                text_ = "\tnurse" + '\t\t'
+                t.insert(INSERT, text_)
+                t.pack()
+                print(text_,end='\t')
+            elif "stretcher" in container.name:
+                text_ = "\tstretcher" + '\t\t'
+                t.insert(INSERT, text_)
+                t.pack()
+                print(text_,end='\t')
             else:
                 text_ = "\t-" + '\t\t'
                 t.insert(INSERT, text_)
@@ -212,7 +296,11 @@ def init_num_devices(client):
     wea_list = client.containers.list(filters={'name': "weather_*"})
     if len(wea_list) != 0: num_devices["weather"] = len(wea_list)
     air_list = client.containers.list(filters={'name': "air_*"})
-    if len(wea_list) != 0: num_devices["air"] = len(air_list)
+    if len(air_list) != 0: num_devices["air"] = len(air_list)
+    nur_list = client.containers.list(filters={'name': "nurse_*"})
+    if len(nur_list) != 0: num_devices["nurse"] = len(nur_list)
+    stre_list = client.containers.list(filters={'name': "stretcher_*"})
+    if len(stre_list) != 0: num_devices["stretcher"] = len(stre_list)
 
 
 def show_stats():
@@ -265,13 +353,23 @@ def show_types():
             t.insert(INSERT, text_)
             t.pack()
             print(text_, end='\t')
-        # if num_devices["airq"] != 0:
-        #     text_ = 'Air quality:' + str(num_devices["air"]) + '\t'
-        #     t.insert(INSERT, text_)
-        #     t.pack()
-        #     print(text_, end='\t')
+        if num_devices["air"] != 0:
+            text_ = 'Air quality:' + str(num_devices["air"]) + '\t'
+            t.insert(INSERT, text_)
+            t.pack()
+            print(text_, end='\t')
+        if num_devices["nurse"] != 0:
+            text_ = 'Nurse:' + str(num_devices["nurse"]) + '\n'
+            t.insert(INSERT, text_)
+            t.pack()
+            print(text_, end='\t')
+        if num_devices["stretcher"] != 0:
+            text_ = 'Stretcher:' + str(num_devices["stretcher"]) + '\n'
+            t.insert(INSERT, text_)
+            t.pack()
+            print(text_, end='\t')
 
-# def button_push(client):
+# def button_push():
 #     print("------------------------------------")
 #     print("Select a patient")
 #     print("------------------------------------")
@@ -327,6 +425,12 @@ def init_num_devices(client):
     if len(smo_list) != 0: num_devices["smoke"] = len(smo_list)
     wea_list = client.containers.list(filters={'name': "weather_*"})
     if len(wea_list) != 0: num_devices["weather"] = len(wea_list)
+    air_list = client.containers.list(filters={'name': "air_*"})
+    if len(air_list) != 0: num_devices["air"] = len(air_list)
+    nur_list = client.containers.list(filters={'name': "nurse_*"})
+    if len(nur_list) != 0: num_devices["nurse"] = len(nur_list)
+    stre_list = client.containers.list(filters={'name': "stretcher_*"})
+    if len(stre_list) != 0: num_devices["stretcher"] = len(stre_list)
 
 
 if __name__ == '__main__':
@@ -350,13 +454,13 @@ if __name__ == '__main__':
 
     # boton4 = Button(window, text="Show containers stats", command=show_stats, height=1, width=22).place(x=10, y=120)
     #
-    # boton5 = Button(window, text="Stop container/s", command=stop_containers, height=1, width=22).place(x=10, y=150)
-    #
+    boton5 = Button(window, text="Stop container/s", command=stopped_containers, height=1, width=22).place(x=10, y=150)
+
     boton6 = Button(window, text="Stop all running containers", command=stop_all, height=1, width=22).place(x=10, y=180)
 
     # boton7 = Button(window, text="Delete all stopped containers", command=runcontainers, height=1, width=22).place(x=10, y=180)
     #
-    # boton8 = Button(window, text="Push emergency button", command=runcontainers, height=1, width=22).place(x=10, y=210)
+    # boton8 = Button(window, text="Push emergency button", command=button_push, height=1, width=22).place(x=10, y=210)
 
     boton9 = Button(window, text="Exit", command=window.destroy, height=1, width=22).place(x=10, y=240)
 
