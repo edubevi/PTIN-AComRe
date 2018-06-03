9f1ebf2fccffaa8aeb2d604132cafce4c9bd892b
eduardbv <eduardberenguer6@gmail.com>
2018-06-03 21:21:25 +0200

    Actualitzada la funció enableDevice

--- libs/send.py
+++ libs/send.py
@@ -1,21 +1,10 @@
 import requests
 import json
-'''
-def createDevice(nametype):
-    req = requests.post('http://ptin2018.herokuapp.com/api/devices',  data = nametype)
-    retorn = json.loads(req.content)
-    vdata = [retorn["id"], retorn["token"]]
-    return vdata
-
-def updateDevice(id, dades, token):
-    headers = {"Authorization": "Bearer "+token}
-    req = requests.put('https://ptin2018.herokuapp.com/api/devices/'+id, data = dades, headers = headers)
-    #print (req.status_code, req.reason)
-'''
 
 #Amb activació
 def createDevice(nametype):
     req = requests.post('http://ptin2018.herokuapp.com/api/devices',  data = nametype)
+    #print(req.status_code,req.reason)
     retorn = json.loads(req.content)
     vdata = [retorn["id"], retorn["token"]]
     return vdata
@@ -23,11 +12,15 @@ def createDevice(nametype):
 def updateDevice(id, dades, token):
     headers = {"Authorization": "Bearer "+token}
     req = requests.put('https://ptin2018.herokuapp.com/api/devices/'+id+'/info', data = dades, headers = headers)
-    #print (req.status_code, req.reason)
+    #print(req.status_code, req.reason)
 
 def enableDevice(id, token):
-    headers = {"Authorization": "Bearer "+token}
-    dades = {'enabled': True, 'deleted': False}
-    req = requests.put('https://ptin2018.herokuapp.com/api/devices'+id, data = dades, headers = headers)
+    dades = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"enabled\"\r\n\r\nTrue\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+    dade = {'enabled' : True }
+
+    caps = {'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+            'Authorization': "Bearer "+token, 'Cache-Control': "no-cache" }
+    response = requests.request("PUT", "https://ptin2018.herokuapp.com/api/devices/"+id, data=dade, headers=caps)
+    #print(response.text)
 
 
